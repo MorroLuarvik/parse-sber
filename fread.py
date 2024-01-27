8a44082fc1db0817e97e12ad1700574c680e9981
Morro Luarvik <dr.morro.l@gmail.com>
2024-01-27 15:24:47 +1000

    parse links

--- fread.py
+++ fread.py
@@ -1,10 +1,29 @@
 import os
 from html.parser import HTMLParser
 
+class MyHTMLParser(HTMLParser):
+    links = []
+
+    def handle_starttag(self, tag, attrs):
+        if (tag != 'script'): return
+        
+        for attr in attrs:
+            if (attr[0] == 'src'): 
+                print(attr)
+                self.links.append(attr[1])
+
 fileName = 'parse/response.html'
 
 filePath = os.path.join(os.path.dirname(__file__), fileName)
 
-
 f = open(filePath, "r")
-print(f.read())
+body = f.read() 
+f.close()
+
+parser = MyHTMLParser()
+parser.feed(body)
+parser.close()
+
+print(parser.links)
+
+#print(feed)
