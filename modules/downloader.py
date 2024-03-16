2e8d7784e42996f8043141455c8b663d64503b7e
Morro Luarvik <dr.morro.l@gmail.com>
2024-03-16 15:49:45 +1000

    download in module

--- modules/downloader.py
+++ modules/downloader.py
@@ -2,6 +2,7 @@ import os
 import requests
 import time
 from html.parser import HTMLParser
+from urllib.parse import urljoin
 
 class MyHTMLParser(HTMLParser):
     links = []
@@ -23,6 +24,7 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 
 def load_files(url: str, download_folder: str) -> list:
     'Скачивает страницу и сохраняет указанные там js файлы'
+
     files = []
     with requests.Session() as session:
         session.headers.update({'User-Agent': USER_AGENT})
@@ -32,26 +34,23 @@ def load_files(url: str, download_folder: str) -> list:
             raise requests.ConnectionError(f'Expected status code 200, but got {format(response.status_code)}')
         
         main_name = time.strftime('%Y.%m.%d_%H.%M.%S')
-        file_name = os.path.join(download_folder, main_name+".html")
-        with open(file_name, "w") as f:
+        file_path = os.path.join(download_folder, main_name+".html")
+        with open(file_path, "w") as f:
             f.write(response.text)
-            """
+            files.append(file_path)
+            
             parser = MyHTMLParser()
             parser.feed(response.text)
             parser.close()
 
             for link in parser.links:
-                partName = link[-2:] + '.js'
-                response = session.get(root_url + link)
+                response = session.get(urljoin(url, link))
                 if response.status_code != 200:
-                        print("Load part", partName)
-                        exit()
-
-                filePath = os.path.join(responsePath, partName)
-                f = open(filePath, "w")
-                f.write(response.text)
-                f.close()
-            """
-
-
-
+                    raise requests.ConnectionError(f'Expected status code 200, but got {format(response.status_code)}')
+                
+                script_name = main_name + '_' + link[-2:] + '.js'
+                script_path = os.path.join(download_folder, script_name)
+                with open(script_path, "w") as f:
+                    f.write(response.text)
+                    files.append(script_path)
+    return files
--- simple.py
+++ simple.py
@@ -1,47 +1,15 @@
 import requests
 import os
 import time
+from urllib.parse import urlparse, urljoin, urlunsplit
 
-url = "https://www.sberbank.ru/ru/person"
-url = "https://sberbank.ru"
-url = "https://ya.ru"
+url = "https://www.sberbank.ru/proxy/services/rates/public/v2/history?rateType=PMR-3&isoCode=A98&date=1705611600000&regionId=070"
 
+#scheme, host, *_ = 
+#up = urlparse(url)
 
-r = requests.get(url)
+#print(up)
 
-print("=" * 50)
-print(r.status_code)
-print("-" * 50)
-print(r.headers)
-print("-" * 50)
-print(r.cookies.get_dict())
+#nur = 
 
-
-print("=" * 50)
-from html.parser import HTMLParser
-
-class MyHTMLParser(HTMLParser):
-    links = []
-
-    def handle_starttag(self, tag, attrs):
-        if tag != 'script': return
-        #if not len(attrs): return
-
-        for link in (val for attr, val in attrs if attr == 'src'):
-            self.links.append(link)
-
-        '''
-        links = self.links
-        map(lambda val: links.append(val), [val for attr, val in attrs if attr == 'src'])
-        print(attrs)
-
-        for attr in attrs:
-            if attr[0] == 'src': 
-                print(attr)
-                self.links.append(attr[1])
-        '''
-
-parser = MyHTMLParser()
-parser.feed(r.text)
-parser.close()
-print(parser.links)
+print(urljoin(url, 'kek'))
